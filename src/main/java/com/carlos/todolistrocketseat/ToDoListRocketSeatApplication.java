79d2f14ff8968f2e33de55767ae78aaa620904a5
Carlos Soler <carlinhoscaco09@gmail.com>
2023-10-11 19:51:29 -0300

    Commit inicial

---
+++ src/main/java/com/carlos/todolistrocketseat/ToDoListRocketSeatApplication.java
@@ -0,0 +1,13 @@
+package com.carlos.todolistrocketseat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListRocketSeatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoListRocketSeatApplication.class, args);
+    }
+
+}
