c0e110de4a5e31228e3954b90301112b6e0c1392
Carlos Soler <carlinhoscaco09@gmail.com>
2023-10-11 21:53:16 -0300

    Add: Adicionado para salvar uma task

--- src/main/java/com/carlos/todolistrocketseat/Controller/TaskController.java
+++ src/main/java/com/carlos/todolistrocketseat/Controller/TaskController.java
@@ -1,4 +1,21 @@
 package com.carlos.todolistrocketseat.Controller;
 
+import com.carlos.todolistrocketseat.Model.Task;
+import com.carlos.todolistrocketseat.Repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/task")
 public class TaskController {
+
+    @Autowired
+    TaskRepository taskRepository;
+
+    @PostMapping
+    public ResponseEntity<Task> createTask(@RequestBody Task task){
+        return ResponseEntity.status(HttpStatus.CREATED).body(taskRepository.save(task));
+    }
 }
