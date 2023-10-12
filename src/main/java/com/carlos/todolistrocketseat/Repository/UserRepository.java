73a9a875e1c97aa4119d88762b46a694f72154f1
Carlos Soler <carlinhoscaco09@gmail.com>
2023-10-12 13:12:23 -0300

    Add: Puxar as tasks do usuário autenticado

--- src/main/java/com/carlos/todolistrocketseat/Repository/UserRepository.java
+++ src/main/java/com/carlos/todolistrocketseat/Repository/UserRepository.java
@@ -12,4 +12,6 @@ public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findUserByUserName(String userName);
     Optional<User> findUserByIdUser(UUID idUser);
+
+
 }
