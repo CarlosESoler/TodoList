ccee0779e95c10f0cecada1631a4c65268187ed7
Carlos Soler <carlinhoscaco09@gmail.com>
2023-10-11 21:37:22 -0300

    Add: Modificando algumas coisas no User

--- src/main/java/com/carlos/todolistrocketseat/Controller/UserController.java
+++ src/main/java/com/carlos/todolistrocketseat/Controller/UserController.java
@@ -4,12 +4,9 @@ import com.carlos.todolistrocketseat.Model.User;
 import com.carlos.todolistrocketseat.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/user")
@@ -19,7 +16,11 @@ public class UserController {
     private UserRepository userRepository;
 
     @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody User user){
+    public ResponseEntity createUser(@RequestBody User user){
+
+        if(userRepository.findUserByUserName(user.getUserName()).isPresent()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists");
+        }
        return ResponseEntity.status(HttpStatus.CREATED).body(userRepository.save(user));
     }
 
--- src/main/java/com/carlos/todolistrocketseat/Model/User.java
+++ src/main/java/com/carlos/todolistrocketseat/Model/User.java
@@ -1,8 +1,12 @@
 package com.carlos.todolistrocketseat.Model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 
 import java.sql.Timestamp;
 import java.util.UUID;
@@ -12,10 +16,9 @@ import java.util.UUID;
 @Table(name = "users")
 public class User {
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID userId;
+    private UUID idUser;
 
     @Column(unique = true)
     private String userName;
@@ -24,4 +27,5 @@ public class User {
 
     @CreationTimestamp
     private Timestamp createdAt;
+
 }
