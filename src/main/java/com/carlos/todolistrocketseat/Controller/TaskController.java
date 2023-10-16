7fd7203890bfbf6f3134f148a6809a1e171a8d18
Carlos Soler <carlinhoscaco09@gmail.com>
2023-10-16 20:30:36 -0300

    Add & refac: Adicionado o filtro para apenas o dono da tarefa alterar a mesma. Modificado alguns metodos tentando aplicar design pattern

--- src/main/java/com/carlos/todolistrocketseat/Controller/TaskController.java
+++ src/main/java/com/carlos/todolistrocketseat/Controller/TaskController.java
@@ -28,38 +28,54 @@ public class TaskController {
         LocalDate startedAt = task.getStartedAt();
         LocalDate endAt = task.getEndAt();
 
-        if(startedAt.isBefore(today) || endAt.isBefore(today)) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início / término não pode ser menor que a data atual");
+        if (startedAt.isBefore(today) || endAt.isBefore(today)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("A data de início / término não pode ser menor que a data atual");
         }
-        if(startedAt.isAfter(task.getEndAt())) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início não pode ser maior que a data de término");
+        if (startedAt.isAfter(task.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("A data de início não pode ser maior que a data de término");
         }
 
         task.setIdUser((UUID) request.getAttribute("idUser"));
-        return ResponseEntity.status(HttpStatus.CREATED).body(taskRepository.save(task));
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(taskRepository.save(task));
     }
 
     @GetMapping
     public ResponseEntity getTasks(HttpServletRequest request) {
-        List<Task> tasks = taskRepository.findByIdUser((UUID) request.getAttribute("idUser"));
-        System.out.println(tasks);
-        return ResponseEntity.status(HttpStatus.OK).body(tasks);
+        List<Task> tasks = taskRepository
+                .findByIdUser((UUID) request.getAttribute("idUser"));
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(tasks);
     }
 
     @GetMapping("/{idTask}")
     public ResponseEntity getTaskById(@PathVariable UUID idTask, HttpServletRequest request) {
-        return ResponseEntity.status(HttpStatus.OK).body(taskRepository.findById(idTask));
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(taskRepository.findById(idTask));
     }
 
     @PutMapping("/{idTask}")
     public ResponseEntity updateTask(@RequestBody Task task, @PathVariable UUID idTask, HttpServletRequest request) {
         Optional<Task> taskT = taskRepository.findById(idTask);
-        if(taskT.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Tarefa não encontrada");
+        if (taskT.isEmpty()) {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body("Tarefa não encontrada");
         }
+
+        Object idUser = request.getAttribute("idUser");
+
+        if (!taskT.get().getIdUser().equals(idUser))
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Você não tem permissão para alterar essa tarefa");
 
         UtilsProp.copyNonNullProperties(task, taskT.get());
 
-        return ResponseEntity.status(HttpStatus.OK).body(taskRepository.save(taskT.get()));
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(taskRepository.save(taskT.get()));
     }
 }
